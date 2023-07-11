8660e9d8be2cc1baeb92690f14de0e2f7170d9d3
GabrielePazi <pazigabriele@gmail.com>
2023-07-11 17:03:18 +0200

    1.1.1 modifiche al bonus

--- bonus/mainBonus.js
+++ bonus/mainBonus.js
@@ -1,68 +1,62 @@
 const btnPlay = document.querySelector(".btn-play");
-const container = document.querySelector(".grid-container");
 
 btnPlay.addEventListener("click", onBtnClick);
 
 function onBtnClick() {
+  const container = document.querySelector(".grid-container");
   const cellQuantity = document.querySelector(".cell-number").value;
 
   container.classList.add("my-3");
   
-  let grid = createGrid(cellQuantity);
-
-  changeColorOnClick(grid)
-
-  printGrid(grid);
+  createGrid(cellQuantity, container);
 }
 
 /**
- * Crea tante celle quante il valore di cellNumber
+ * Crea una grid e la stampa in html
  */
-function createGrid(cellNumber) {
+function createGrid(cellNumber, container) {
   let grid = [];
-
+  
   for (let i = 0; i < cellNumber; i++) {
+    //crea una singola casella
     let singleCell = createCell(cellNumber);
     singleCell.innerHTML = i + 1;
+
+    //quando clicchi una cella diventa blu e stampa il numero della cella in console
+    singleCell.addEventListener("click", function() {
+      singleCell.classList.toggle("bg-primary");
+
+      console.log("Hai cliccato la casella ", i + 1);
+    })
     
     grid.push(singleCell);
   }
+
+  //stampa la griglia
+  printGrid(grid, container);
 
   return grid;
 }
 
 /**
- * Crea una singola cella, creando un nuovo div
+ * Crea una singola cella
  */
 function createCell(cellDimension) {
   let cell = document.createElement("div");
-
-  const dimension = Math.sqrt(cellDimension)
-  cell.style.flexBasis = `calc(100% / ${dimension})`
+ 
+  cell.style.width = `calc(100% / ${Math.sqrt(cellDimension)})`;
   cell.classList.add("single-cell");
 
   return cell;
 }
 
 /**
- * Prende in input un array di HTMLDivElement e li stampa in html
+ * Prende in input un array di HTMLDivElement e li appende al container che viene svuotato inizialmente
  */
-function printGrid(grid) {
+function printGrid(grid, container) {
   container.innerHTML = "";
 
   for (let i = 0; i < grid.length; i++) {
-    container.append(grid[i])    
-  }
-}
-
-function changeColorOnClick(grid) {
-  for (let i = 0; i < grid.length; i++) {
-    const cell = grid[i];
-
-    cell.addEventListener("click", function() {
-      cell.classList.toggle("bg-primary");
-
-      console.log("Hai cliccato la casella ", i + 1);
-    })
+    container.append(grid[i]);    
   }
 }
